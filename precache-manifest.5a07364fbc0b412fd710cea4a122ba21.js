0ba0993b085615df10436bebd29c126e19441d63
Vladimir Zemskov <vlzemskovs@gmail.com>
2019-06-09 16:36:37 +0300

    init

---
+++ precache-manifest.5a07364fbc0b412fd710cea4a122ba21.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7e78d657355fe8a776836270ed6c90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b499e4fb6983779780dc",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "d5e7e689e790dd5f60eaa438da65581f",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "61bbf26d99172b62fb8eb5b03585db72",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "0cefb5cc6ff6df9509ed193cff79de7c",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "0a51e7bba83f6ce6ef65b35a9ba4e0f2",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "b499e4fb6983779780dc",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "67c874abf58c143de3c698e0ff393632",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b08f600e75284d6274ee0ee03f2ed1b3",
+    "url": "static\\icons\\128x128.png"
+  },
+  {
+    "revision": "78bb1bad56b56f9176976fbb19f640d8",
+    "url": "static\\icons\\144x144.png"
+  },
+  {
+    "revision": "09bac65cbc73f494482c480006f7c31a",
+    "url": "static\\icons\\152x152.png"
+  },
+  {
+    "revision": "1c41296078623fe115c3b966852a9759",
+    "url": "static\\icons\\192x192.png"
+  },
+  {
+    "revision": "ad809d0a19d880fc189e41e9726df58e",
+    "url": "static\\icons\\256x256.png"
+  },
+  {
+    "revision": "9fa40ac188452505f216044f8c7a9b88",
+    "url": "static\\icons\\512x512.png"
+  },
+  {
+    "revision": "14235e3018bb57cc700c799c5f9ef1be",
+    "url": "static\\icons\\apple-touch-icon.png"
+  },
+  {
+    "revision": "c88219dd9796cca0bfa90e472a805ab9",
+    "url": "static\\icons\\favicon.png"
+  }
+]);
